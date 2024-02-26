3f8128d047f923891b009a1e889cb597f1d1997b
juan <juanpisoo40@gmail.com>
2024-02-26 07:55:53 -0500

    form arreglado

--- orders/views.py
+++ orders/views.py
@@ -7,7 +7,7 @@ import json
 from .models import Payment, Product
 from django.template.loader import render_to_string
 from django.http import JsonResponse
-
+from django.contrib import messages
 
 # Create your views here.
 
@@ -62,7 +62,7 @@ def payments(request):
     print(data)
     return JsonResponse(data)
 
-def place_order(request, total = 0, quantity = 0):
+def place_order(request, total=0, quantity=0):
     current_user = request.user
     cart_items = CartItem.objects.filter(user=current_user)
     cart_count = cart_items.count()
@@ -76,26 +76,16 @@ def place_order(request, total = 0, quantity = 0):
     for cart_item in cart_items:
         total += (cart_item.product.price * cart_item.quantity)
         quantity += cart_item.quantity
-    
-    tax = (2*total)/100
+
+    tax = (2 * total) / 100
     grand_total = total + tax
 
     if request.method == 'POST':
         form = OrderForm(request.POST)
 
         if form.is_valid():
-            data = Order()
+            data = form.save(commit=False)
             data.user = current_user
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.phone = form.cleaned_data['phone']
-            data.email = form.cleaned_data['email']
-            data.address_line_1 = form.cleaned_data['address_line_1']
-            data.address_line_2 = form.cleaned_data['address_line_2']
-            data.state = form.cleaned_data['state']
-            data.city = form.cleaned_data['city']
-            data.country = form.cleaned_data['country']
-            data.order_note = form.cleaned_data['order_note']
             data.order_total = grand_total
             data.tax = tax
             data.ip = request.META.get('REMOTE_ADDR')
@@ -115,13 +105,16 @@ def place_order(request, total = 0, quantity = 0):
 
             order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
             context = {
-                'order' : order,
-                'cart_items' : cart_items,
-                'total' : total,
-                'tax' : tax,
-                'grand_total' : grand_total,
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
             }
             return render(request, 'orders/payments.html', context)
+        else:
+            messages.error(request, 'Por favor completa todos los campos obligatorios.')
+            return redirect('checkout')
     else:
         return redirect('checkout')
 
